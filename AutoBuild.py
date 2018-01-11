b04b72eef60b5047ab5bd12cd600e9ca10304d34
sunjinshuai <s_mike@163.com>
2018-01-11 15:36:46 +0800

    python自动打包

--- AutoBuild.py
+++ AutoBuild.py
@@ -11,13 +11,12 @@ import smtplib
 import sendMail
 import ConfigParser
 
-
 # 发邮件
 def send_mail():
     sendMail.send_mail();
 
 # 导出xcarchive,ipa
-def build_project(conf,bundleID,sign,pName,plistPath):
+def build_project(conf,bundleID,plistPath):
 
     timeName = time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime(time.time()))
 
@@ -25,7 +24,7 @@ def build_project(conf,bundleID,sign,pName,plistPath):
     xcarchivePath = '%s/%s/%s_%s.xcarchive' %(conf['targerIPA_path'],timeName,conf['project_name'],conf['type'])
 
     # 导出xcarchive
-    build = 'xcodebuild -workspace %s -scheme %s -configuration %s -archivePath %s clean archive build CODE_SIGN_IDENTITY="%s" PROVISIONING_PROFILE="%s" PRODUCT_BUNDLE_IDENTIFIER="%s"' %(xcworkPath,conf['project_name'],conf['configuration'],xcarchivePath,sign,pName,bundleID)
+    build = 'xcodebuild -workspace %s -scheme %s -configuration %s -archivePath %s clean archive build PRODUCT_BUNDLE_IDENTIFIER="%s"' %(xcworkPath,conf['project_name'],conf['configuration'],xcarchivePath,bundleID)
     print(build);
     os.system(build)
 
@@ -42,13 +41,11 @@ def build_project(conf,bundleID,sign,pName,plistPath):
     filePath = '%s/%s' %(conf['targerIPA_path'],timeName)
     if conf['index'] is 0 or conf['index'] is 3 and conf['needUpload']:
         uploadIPA = '%s/%s.ipa' % (filePath,conf['project_name'])
-        os.system('chmod  u+x %s/UploadIPA.sh'%(get_path()))
+        os.system('chmod u+x %s/UploadIPA.sh'%(get_path()))
         os.system('bash %s/UploadIPA.sh %s'%(get_path(),uploadIPA))
         pass
     else:
         os.system('open %s'%(filePath))
-
-
 
 def get_path():
     path = sys.path[0]
@@ -61,8 +58,13 @@ def get_build_project_data():
     cf = ConfigParser.ConfigParser()
     cf.read('%s/conf.ini' % get_path())
 
-    conf ={'project_path':cf.get('conf', 'project_path'),'project_name':cf.get('conf', 'Project_Name'),'workspace_Name':cf.get('conf', 'Workspace_Name'),'targerIPA_path':cf.get('conf', 'targerIPA_path'),'configuration':cf.get('conf', 'Configuration')}
-    conf['needSendMail']=cf.getboolean('conf', 'needSendMail');
+    conf = {'project_path':cf.get('conf', 'project_path'),
+            'project_name':cf.get('conf', 'Project_Name'),
+          'workspace_Name':cf.get('conf', 'Workspace_Name'),
+          'targerIPA_path':cf.get('conf', 'targerIPA_path'),
+           'configuration':cf.get('conf', 'Configuration')}
+
+    conf['needSendMail'] = cf.getboolean('conf', 'needSendMail');
     conf['needUpload'] = cf.get('conf','needUpload');
 
     list = ["AdHoc","AppStore","Enterprise","Development"]
@@ -78,16 +80,13 @@ def get_build_project_data():
         conf['type'] = key;
         conf['index'] = index;
 
-        build_project(conf,cf.get(key,'BundleID'),cf.get(key,'SIGN_IDENTITY'),cf.get(key,'PROVISIONING_PROFILE_NAME'),cf.get(key,'PlistPath'))
+        build_project(conf,cf.get(key,'BundleID'),cf.get(key,'PlistPath'))
     else:
         print("无效序号!")
-
-
 
 def main():
     # 从conf.ini文件中得到数据并开始编译导出
     get_build_project_data();
-
 
 # 执行
 main()
